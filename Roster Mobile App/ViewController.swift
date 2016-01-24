96b25c7038fef11d83eff1f0f78584eca804da0a
Arshjot Singh <arsh.hunjan@gmail.com>
2016-01-24 15:37:58 -0500

    Initial Commit

---
+++ Roster Mobile App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Roster Mobile App
+//
+//  Created by Student on 2016-01-24.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
